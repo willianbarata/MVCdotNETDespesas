7dea0c579ed02785da8704d649396738e18a9651
willianbarata <willianbarata@gmail.com>
2022-05-15 11:50:27 -0300

    Crud básico

---
+++ ExpensiveControlApp/DTOs/CreateExpensiveDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensiveControlApp.DTOs
+{
+    public class CreateExpensiveDTO
+    {
+        public CreateExpensiveDTO()
+        {
+            Date = DateTime.Now;
+        }
+
+        [Required(ErrorMessage = "Descrição é Obrigatória")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Valor é Obrigatório")]
+        [Range(0.01, 99999999, ErrorMessage = "Valor deve ser maior que zero")]
+        public double Value { get; set; }
+
+        [Required(ErrorMessage = "Data é obrigatória")]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ ExpensiveControlApp/Models/Expensives/Expensive.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensiveControlApp.Models.Expensives
+{
+    public class Expensive
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        public double Value { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
